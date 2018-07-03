8e20fe504bdcf28c59d43865cfc9458c7dff8113
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-03 21:29:05 +1000

    Update Account type

--- schema/types/account.js
+++ schema/types/account.js
@@ -10,10 +10,11 @@ module.exports = new GraphQLObjectType({
   name: 'Account',
   fields: () => {
     return {
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      sla__c: { type: GraphQLString },
-      description: { type: GraphQLString }
+      Id: { type: GraphQLID },
+      Name: { type: GraphQLString },
+      SLA__c: { type: GraphQLString },
+      Description: { type: GraphQLString },
+      ExtId__c: {type: GraphQLString }
     }
   }
 })
