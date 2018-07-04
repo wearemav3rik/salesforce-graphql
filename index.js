01a4d3a96992348b4a2287893eae59693c783d11
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-04 22:12:23 +1000

    Fix jshint errors

--- index.js
+++ index.js
@@ -22,25 +22,22 @@ const org = connectToOrg({
 const salesforce = require('./database/salesforce')(org)
 
 // Authenticate to salesforce
-try {
-  (async () => {
-    await authenticate(
-      org, process.env.SALESFORCE_USERNAME,
-      process.env.SALESFORCE_PASSWORD+process.env.SALESFORCE_SECURITY_TOKEN
-    )
-    // GraphQL middleware
-    server.use('/graphql', (req, res) => {
-      // TODO: Use facebook dataloader to cache queries
-      graphqlHTTP({
-        schema: rootSchema,
-        graphiql: true,
-        context: { salesforce }
-      })(req, res)
-    })
-  })()
-} catch (error) {
+authenticate(
+  org, process.env.SALESFORCE_USERNAME,
+  process.env.SALESFORCE_PASSWORD+process.env.SALESFORCE_SECURITY_TOKEN
+).then(response => {
+  // GraphQL middleware
+  server.use('/graphql', (req, res) => {
+    // TODO: Use facebook dataloader to cache queries
+    graphqlHTTP({
+      schema: rootSchema,
+      graphiql: true,
+      context: { salesforce }
+    })(req, res)
+  })
+}).catch(error => {
   assert.equal(error, null)
-}
+})
 
 // Error handler
 server.use((error, req, res, next) => {
