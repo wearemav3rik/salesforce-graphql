d811037a69d7658116f94b8b71ea61fb0fdb9f45
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-05 22:28:54 +1000

    Add createMultipleAccounts function

--- database/salesforce.js
+++ database/salesforce.js
@@ -12,7 +12,7 @@ module.exports = org => {
     async getAccounts() {
       const response = await query(
         org,
-        `select Id, Name, Description, SLA__c from Account`
+        `select Id, Name, Description, SLA__c, ExtId__c from Account`
       )
       return response.records
     },
@@ -20,7 +20,7 @@ module.exports = org => {
     async getAccountById(id) {
       const response = await query(
         org,
-        `select Id, Name, Description, SLA__c from Account WHERE Id = '${id}'`
+        `select Id, Name, Description, SLA__c, ExtId__c from Account WHERE Id = '${id}'`
       )
       return response.records
     },
@@ -28,7 +28,7 @@ module.exports = org => {
     async getAccountByName(name) {
       const response = await query(
         org,
-        `select Id, Name, Description, SLA__c from Account WHERE Name = '${name}'`
+        `select Id, Name, Description, SLA__c, ExtId__c from Account WHERE Name = '${name}'`
       )
       return response.records
     },
@@ -54,6 +54,16 @@ module.exports = org => {
       const response = await upsertRecord(org, 'Account', record, externalIdFieldName)
       record.Id = response.id
       return record
+    },
+
+    async createMultipleAccounts(records) {
+      const response = await createRecord(org, 'Account', records)
+      console.log(response)
+      const recordsWithId = records.map((record, index) => {
+        record.Id = response[index].id
+        return record
+      })
+      return recordsWithId
     }
   }
 }
