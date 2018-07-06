102e3cd45d2348afa66aff5aa7c3aae0950a998e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-06 17:03:21 +1000

    Add update multiple accounts mutation

---
+++ schema/mutations/update-multiple-accounts.js
@@ -0,0 +1,21 @@
+const {
+  GraphQLInputObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList
+} = require('graphql')
+
+
+const AccountType = require('../types/account')
+
+const AccountInputType = require('../inputs/account-input')
+
+module.exports = {
+  type: GraphQLList(AccountType),
+  args: {
+    input: { type: new GraphQLNonNull(new GraphQLList(AccountInputType)) }
+  },
+  resolve: (obj, { input }, { salesforce }) => {
+    return salesforce.updateMultipleAccounts(input)
+  }
+}
