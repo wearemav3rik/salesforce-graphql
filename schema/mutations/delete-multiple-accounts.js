2bc8137fbcc7bd9fbd6e0ab60826b58c2c1f73ef
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-07 17:20:46 +1000

    Add delete multiple accounts mutation

---
+++ schema/mutations/delete-multiple-accounts.js
@@ -0,0 +1,22 @@
+const {
+  GraphQLInputObjectType,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLID
+} = require('graphql')
+
+
+const AccountType = require('../types/account')
+
+// const AccountInputType = require('../inputs/account-input')
+
+module.exports = {
+  type: GraphQLList(AccountType),
+  args: {
+    Ids: { type: new GraphQLNonNull(new GraphQLList(GraphQLID)) }
+  },
+  resolve: (obj, { Ids }, { salesforce }) => {
+    return salesforce.deleteMultipleAccounts(Ids)
+  }
+}
