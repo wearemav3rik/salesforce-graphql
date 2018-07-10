decfd7e70379673742f70a500ecfb61f49dbbf5e
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-07-10 17:24:05 +1000

    Add UpsertMultipleAccountMutation

--- schema/root.js
+++ schema/root.js
@@ -54,6 +54,7 @@ const UpsertAccountMutation = require('./mutations/upsert-account')
 const AddMultipleAccountMutation = require('./mutations/add-multiple-accounts')
 const UpdateMultipleAccountMutation = require('./mutations/update-multiple-accounts')
 const DeleteMultipleAccountMutation = require('./mutations/delete-multiple-accounts')
+const UpsertMultipleAccountMutation = require('./mutations/upsert-multiple-accounts')
 
 const RootMutationType = new GraphQLObjectType({
   name: 'RootMutation',
@@ -65,7 +66,8 @@ const RootMutationType = new GraphQLObjectType({
     UpsertAccountByExternalID: UpsertAccountMutation,
     AddMultipleAccountMutation: AddMultipleAccountMutation,
     UpdateMultipleAccountMutation: UpdateMultipleAccountMutation,
-    DeleteMultipleAccountMutation: DeleteMultipleAccountMutation
+    DeleteMultipleAccountMutation: DeleteMultipleAccountMutation,
+    UpsertMultipleAccountMutation: UpsertMultipleAccountMutation
   })
 })
 
